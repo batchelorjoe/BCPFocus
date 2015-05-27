4539c8ac5926f2d647b3b8e9170254b4fd869bdd
batchelorjoe <batchelorjoe@gmail.com>
2015-05-27 10:38:42 -0500

    Added persist

--- app/src/main/java/com/foreverlifestyle/bcpfocus/FocusActivity.java
+++ app/src/main/java/com/foreverlifestyle/bcpfocus/FocusActivity.java
@@ -4,9 +4,25 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+
+import java.io.File;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 
@@ -23,13 +39,87 @@ public class FocusActivity extends ActionBarActivity {
         setContentView(R.layout.activity_focus);
         lvItems = (ListView) findViewById(R.id.lvItems);
         items = new ArrayList<String>();
+        readItems();
         itemsAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, items);
         lvItems.setAdapter(itemsAdapter);
         items.add("First Item");
         items.add("Second Item");
         items.add("Third Item");
+        setupListViewListener();
+
+    }
+    private void setupListViewListener() {
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                items.remove(position);
+                itemsAdapter.notifyDataSetChanged();
+                saveItems();
+                return true;
+            }
+        });
+
+    }
+
+    private void readItems() {
+        File filesDir = getFilesDir();
+        File focusFile = new File(filesDir, ".bcp.txt");
+
+        try {
+            items = new ArrayList<String>(FileUtils.readLines(focusFile));
+        } catch (IOException e) {
+            items = new ArrayList<String>();
+            e.printStackTrace();
+        }
+/*
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpContext localContext = new BasicHttpContext();
+        HttpGet httpGet = new HttpGet("http://www.foreverlifestyle.com/index_1.php?format=text&method=hello");
+        String text = null;
+        try {
+            HttpResponse response = httpClient.execute(httpGet, localContext);
+            HttpEntity entity = response.getEntity();
+            text = getASCIIContentFromEntity(entity);
+            items.add(text);
+
+        } catch (Exception e) {
+            items.add("No Item");
+        }
+
+  */
+    }
 
 
+
+     private String getASCIIContentFromEntity(HttpEntity entity) throws IllegalStateException, IOException {
+        InputStream in = entity.getContent();
+        StringBuffer out = new StringBuffer();
+        int n = 1;
+        while (n>0) {
+            byte[] b = new byte[4096];
+            n = in.read(b);
+            if (n>0) out.append(new String(b,0,n));
+
+        }
+        return out.toString();
+    }
+
+
+    private void  saveItems() {
+        File filesDir = getFilesDir();
+        File focusFile = new File(filesDir, ".bcp.txt");
+        try {
+            FileUtils.writeLines(focusFile, items);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void addFocusItem(View view) {
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        itemsAdapter.add(etNewItem.getText().toString());
+        etNewItem.setText("");
+        saveItems();
 
 
     }
